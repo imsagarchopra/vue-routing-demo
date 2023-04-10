db743dad8bd98b6941ec8e04e7757ad7f950161e
Sagar Chopra <schopra@firemate.com>
2023-04-10 16:50:53 +0530

    Fetching data from database

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,18 +1,29 @@
 <template>
-    <div>
-        <h1>Job Details Page</h1>
+    <div v-if="job">
+        <h1>{{job.title}}</h1>
         <p>The job id is {{ id }}</p>
+        <p>{{job.details}}</p>      
+    </div>
+    <div v-else>
+        Loading ......
     </div>
 </template>
 
 <script>
     export default {
-        props: ['id'] 
-        // data(){
-        //     return{
-        //         id: this.$route.params.id
-        //     }
-        // }
+        props: ['id'] ,
+        data(){
+            return{
+                //id: this.$route.params.id
+                job: null
+            }
+        },
+        mounted(){
+            fetch('http://localhost:3000/jobs/' + this.id)
+            .then(response => response.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
+        }
     }
 </script>
 
--- src/views/Jobs/Jobs.vue
+++ src/views/Jobs/Jobs.vue
@@ -9,12 +9,20 @@
     export default {
         data(){
             return{
-                jobs:[
-                    { title: 'UX Designer', id: 1, details: 'I am a UX Designer'},
-                    { title: 'Web Developer', id: 2, details: 'I am a Web Developer'},
-                    { title: 'Vue Developer', id: 3, details: 'I am a Vue Developer'},
-                ]
+                // jobs:[
+                //     { title: 'UX Designer', id: 1, details: 'I am a UX Designer'},
+                //     { title: 'Web Developer', id: 2, details: 'I am a Web Developer'},
+                //     { title: 'Vue Developer', id: 3, details: 'I am a Vue Developer'},
+                // ]
+
+                jobs:[]
             }
+        },
+        mounted(){
+            fetch('http://localhost:3000/jobs')
+            .then(response => response.json())
+            .then(data => this.jobs = data)
+            .catch(err => console.log(err.message))
         }
     }
 </script>
