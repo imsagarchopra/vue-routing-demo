9007f5988586137a398de838bf676e4aca001f40
Sagar Chopra <schopra@firemate.com>
2023-04-10 16:30:27 +0530

    Vue Routing Demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Jobs from '../views/Jobs/Jobs.vue'
+import JobDetails from '../views/Jobs/JobDetails.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 
 const routes = [
   {
@@ -13,7 +17,27 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobDetails',
+    component: JobDetails,
+    props: true
+  },
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'pageNotFound',
+    component: PageNotFound
   }
 ]
 
---
+++ src/views/Jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <router-link :to="{name: 'jobDetails', params:{id: job.id}}"> <h2>{{ job.title }}</h2></router-link>  
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                jobs:[
+                    { title: 'UX Designer', id: 1, details: 'I am a UX Designer'},
+                    { title: 'Web Developer', id: 2, details: 'I am a Web Developer'},
+                    { title: 'Vue Developer', id: 3, details: 'I am a Vue Developer'},
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
